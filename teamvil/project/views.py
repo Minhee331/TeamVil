5c0117bf453912aa1ba238b8f4b4a3bc5e3d2fe0
whwkdns13 <whwkdns13@naver.com>
2021-06-30 19:52:12 +0900

    210630_네브바설정중

--- teamvil/project/views.py
+++ teamvil/project/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def team_detail(request):
+    return render(request, "team_detail.html")
--- teamvil/teamvil/urls.py
+++ teamvil/teamvil/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import project.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('team_detail/', project.views.team_detail, ),
 ]
