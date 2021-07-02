1f470835efc99e1097464635d718bd3229a6c5b5
HD152513 <qag331@naver.com>
2021-07-03 02:01:13 +0900

    210702 -백 팀&멤버 리스트 필터링 작업

--- teamvil/account/views.py
+++ teamvil/account/views.py
@@ -5,4 +5,10 @@ import datetime
 
 def member_search_back(request):
     profiles = Profile.objects.all()
-    return render(request, "member_search_back.html", {'profiles':profiles})
+    field1 = Field1.objects.all() # 대분야 (ex IT)
+    mbti = Mbti.objects.all()
+    region2 = Region2.objects.all() # ~시 (서울만 ~구)
+    term = Term.objects.all()
+    job = Job.objects.all()
+    return render(request, "member_search_back.html", {'profiles':profiles, "field1s":field1, "mbtis" : mbti, 
+                                                    "region2s": region2, "terms": term, "jobs": job})
--- teamvil/project/views.py
+++ teamvil/project/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from home.models import *
+from account.models import *
+
 # Create your views here.
 def team_detail(request):
     return render(request, "team_detail.html")
@@ -9,4 +12,10 @@ def team_search(request):
 
 def team_search_back(request):
     projects = Project.objects.all()
-    return render(request, "team_search_back.html", {'projects':projects})
+    field1 = Field1.objects.all() # 대분야 (ex IT)
+    mbti = Mbti.objects.all()
+    region2 = Region2.objects.all() # ~시 (서울만 ~구)
+    term = Term.objects.all()
+    job = Job.objects.all()
+    return render(request, "team_search_back.html", {'projects':projects, "field1s":field1, "mbtis" : mbti, 
+                                                    "region2s": region2, "terms": term, "jobs": job})
