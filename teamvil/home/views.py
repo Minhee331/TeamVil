4901e5ebb25c62c55fdd7f7d8d7223a10fd39373
HD152513 <qag331@naver.com>
2021-07-04 19:34:26 +0900

    210704 -민희 더미데이터 생성,메인페이지 수정

--- teamvil/home/views.py
+++ teamvil/home/views.py
@@ -4,8 +4,8 @@ from account.models import *
 
 # Create your views here.
 def home(request):
-    projects = Project.objects.all()
-    profiles = Profile.objects.all()
+    projects = Project.objects.all().order_by('-id')[:6]
+    profiles = Profile.objects.all().order_by('-id')[:4]
     return render(request, "home.html", {'profiles':profiles, "projects":projects })
 
 def home_back(request):
--- teamvil/project/urls.py
+++ teamvil/project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('team_detail/', views.team_detail, name = "team_detail"),
+    path('team_detail/<int:project_id>', views.team_detail, name = "team_detail"),
     path('team_search/', views.team_search, name = "team_search"),
     path('team_search_back/', views.team_search_back, name = "team_search_back"),
     path('team_detail_back/<int:project_id>', views.team_detail_back, name = "team_detail_back"),
